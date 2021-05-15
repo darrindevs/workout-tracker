dd595a36844fae47034bd36cce4bbdd1b58d17b0
Zaino <hello@darrin.dev>
2021-05-14 17:54:22 -0700

    update routes/api

--- routes/api.js
+++ routes/api.js
@@ -38,20 +38,27 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 //get the dashboard using /api/workouts/range
+// get get aggregate data
+//https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/
 router.get("/api/workouts/range", (req, res) => {
-
-  Workout.find({}).then(dbWorkout => {
-      console.log("ALL WORKOUTS");
-      console.log(dbWorkout);
-
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({_id: -1})
+    .limit(7)
+    
+    .then((dbWorkout) => {
       res.json(dbWorkout);
-  }).catch(err => {
-      res.json(err);
-  });
-
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
-
-
-
 
 module.exports = router;
